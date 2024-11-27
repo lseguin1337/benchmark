9b88e17849d1c0e4e6499d848c4f7f8efa9267d8
Lionel Seguin <lionel.seguin@contentsquare.com>
2024-11-27 13:39:00 +0100

    chore: bundle gain

--- print-result.js
+++ print-result.js
@@ -2,12 +2,16 @@ const fs = require('fs');
 
 const tests = fs.readdirSync(__dirname + '/dist').map((file) => `${__dirname}/dist/${file}`);
 
+function gain(origin, dest) {
+  return (((1 - (dest / origin)) * 100) * -1).toFixed(2) + '%';
+}
+
 for (const test of tests) {
   const es5 = fs.statSync(`${test}/bundle-es5.js`).size;
   const es6 = fs.statSync(`${test}/bundle-es6.js`).size;
   const es2017 = fs.statSync(`${test}/bundle-es2017.js`).size;
   console.log({
     test,
-    bundleSizes: { es5, es6, es2017, percent: ((es5 / es2017) * 100).toFixed(2) + '%' },
+    bundleSizes: { es5, es6, es2017, gainEs5vsEs2017: gain(es5, es2017), gainEs5vsEs6: gain(es5, es6) },
   });
 }
